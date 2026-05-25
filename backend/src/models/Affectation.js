b0a8c91fcf1aa4b2b5740aa47e5ac1e186834c51
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-05-25 10:01:29 +0100

    création du modèle Affectation avec schéma Mongoose

---
+++ backend/src/models/Affectation.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const affectationSchema = new mongoose.Schema(
+    {
+        courrierId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Courrier',
+            required: true
+        },
+        fromUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        serviceId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Service'
+        },
+        statutReception: {
+            type: String,
+            enum: ['EN_ATTENTE', 'ACCEPTE'],
+            default: 'EN_ATTENTE'
+        },
+        commentaire: String,
+        dateAffectation: {
+            type: Date,
+            default: Date.now()
+        },
+        dateReception: Date,
+
+
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model('Affectation', affectationSchema);
