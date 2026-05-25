fa0a664aac817d0227e4bbe6cefbf7eb4f50d292
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-05-25 09:54:41 +0100

    initialisation du serveur avec connexion à la base de données

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+
+    app.listen(PORT, () => {
+        console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    });
+
+});
