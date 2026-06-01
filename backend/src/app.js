c63040f6c943490520d2ebf13df2be3203f57185
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-01 11:32:59 +0100

    configuration des routes d'authentification et d'administration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const adminUserRoutes = require('./routes/adminUserRoutes');
 
 app.use(express.json());
 // routes
@@ -10,5 +13,16 @@ app.get("/", (req, res) => {
         message: "API Gestion des Courriers",
     });
 });
+
+// auth
+app.use('/api/auth', authRoutes);
+// admin
+app.use('/api/admin/users', adminUserRoutes);
+
+
+
+// error handling
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
