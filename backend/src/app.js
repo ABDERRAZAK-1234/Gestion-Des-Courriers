38e4da64a6b53f7df9f070d85a0dbd1e20fab89a
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-02 11:38:20 +0100

    ajout des routes d'administration des courriers

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ const authRoutes = require('./routes/authRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const adminUserRoutes = require('./routes/adminUserRoutes');
 
+const adminCourrierRoutes = require('./routes/adminCourrierRoutes');
+
 app.use(express.json());
 // routes
 app.use("/api/courriers", require("./routes/courriers"));
@@ -18,11 +20,16 @@ app.get("/", (req, res) => {
 app.use('/api/auth', authRoutes);
 // admin
 app.use('/api/admin/users', adminUserRoutes);
+// admin courrier
+app.use('/api/admin/courriers', adminCourrierRoutes);
 
 
 
 // error handling
 app.use(notFound);
 app.use(errorHandler);
+
+
+
 
 module.exports = app;
