7df87b2aebf693c57e3700dbd4a8e4b8fa525bce
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-05 12:35:29 +0100

    ajout de la route de création des courriers sortants

--- backend/src/routes/courriers.js
+++ backend/src/routes/courriers.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { protect } = require('../middlewares/authMiddleware');
 const { authorizeRoles } = require('../middlewares/roleMiddleware');
 const {
-    createIncomingCourrier
+    createIncomingCourrier,
+    createOutgoingCourrier
 } = require('../controllers/courrierController');
 
 const router = express.Router();
 
 router.post('/entrant', protect, authorizeRoles('SECRETAIRE'), createIncomingCourrier);
+router.post('/sortant', protect, authorizeRoles('SECRETAIRE'), createOutgoingCourrier);
 
 router.get('/', (req, res) => {
     res.json({
