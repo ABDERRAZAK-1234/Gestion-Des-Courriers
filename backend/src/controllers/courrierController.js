336aaf96185570c75f3a0a39223f626a59b94256
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-05 12:34:33 +0100

    ajout de la prise en charge des courriers sortants

--- backend/src/controllers/courrierController.js
+++ backend/src/controllers/courrierController.js
@@ -1,7 +1,7 @@
 const Courrier = require('../models/Courrier');
 require('../models/User');
 
-const createIncomingCourrier = async (req, res) => {
+const createCourrierByType = async (req, res, type) => {
     try {
         const {
             reference,
@@ -27,7 +27,7 @@ const createIncomingCourrier = async (req, res) => {
 
         const courrier = await Courrier.create({
             reference,
-            type: 'ENTRANT',
+            type,
             objet,
             description,
             nameFile,
@@ -40,17 +40,26 @@ const createIncomingCourrier = async (req, res) => {
             .populate('serviceId');
 
         return res.status(201).json({
-            message: 'Incoming courrier created successfully',
+            message: `${type === 'ENTRANT' ? 'Incoming' : 'Outgoing'} courrier created successfully`,
             courrier: createdCourrier
         });
     } catch (error) {
         return res.status(500).json({
-            message: 'Failed to create incoming courrier',
+            message: `Failed to create ${type === 'ENTRANT' ? 'incoming' : 'outgoing'} courrier`,
             error: error.message
         });
     }
 };
 
+const createIncomingCourrier = async (req, res) => {
+    return createCourrierByType(req, res, 'ENTRANT');
+};
+
+const createOutgoingCourrier = async (req, res) => {
+    return createCourrierByType(req, res, 'SORTANT');
+};
+
 module.exports = {
-    createIncomingCourrier
+    createIncomingCourrier,
+    createOutgoingCourrier
 };
