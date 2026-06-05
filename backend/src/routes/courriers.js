206800b648e9239aa2bff1aaa6b9a0ea8244e033
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-05 12:17:15 +0100

    Sécurisation de l'enregistrement des courriers entrants pour les secrétaires

--- backend/src/routes/courriers.js
+++ backend/src/routes/courriers.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const { protect } = require('../middlewares/authMiddleware');
+const { authorizeRoles } = require('../middlewares/roleMiddleware');
+const {
+    createIncomingCourrier
+} = require('../controllers/courrierController');
 
 const router = express.Router();
+
+router.post('/entrant', protect, authorizeRoles('SECRETAIRE'), createIncomingCourrier);
 
 router.get('/', (req, res) => {
     res.json({
