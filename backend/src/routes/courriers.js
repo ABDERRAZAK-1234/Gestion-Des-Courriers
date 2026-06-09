280ba77198ebf1557a18b5eb3eb118c49ce0074d
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-09 12:15:23 +0100

    Ajout du route de la recherche et du filtrage

--- backend/src/routes/courriers.js
+++ backend/src/routes/courriers.js
@@ -3,13 +3,15 @@ const { protect } = require('../middlewares/authMiddleware');
 const { authorizeRoles } = require('../middlewares/roleMiddleware');
 const {
     createIncomingCourrier,
-    createOutgoingCourrier
+    createOutgoingCourrier,
+    getAllCourriers
 } = require('../controllers/courrierController');
 
 const router = express.Router();
 
 router.post('/entrant', protect, authorizeRoles('SECRETAIRE'), createIncomingCourrier);
 router.post('/sortant', protect, authorizeRoles('SECRETAIRE'), createOutgoingCourrier);
+router.get('/', protect, getAllCourriers);
 
 router.get('/', (req, res) => {
     res.json({
