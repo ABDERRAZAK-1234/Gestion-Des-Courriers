5bed4f84218e872a9abf295f7909589679cebc1d
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-16 11:59:07 +0100

    Ajouter le middleware d'upload des fichiers courrier

---
+++ backend/src/middlewares/uploadMiddleware.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../../uploads/courriers');
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueName);
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 20 * 1024 * 1024
+    }
+});
+
+module.exports = {
+    upload
+};
