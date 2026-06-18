86b9c8ee25790d08e448b8c9b116ace5e673ace4
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-18 13:00:24 +0100

    Ajouter la route admin d'association des courriers aux services

--- backend/src/routes/adminCourrierRoutes.js
+++ backend/src/routes/adminCourrierRoutes.js
@@ -2,12 +2,16 @@ const express = require('express');
 const { protect } = require('../middlewares/authMiddleware');
 const { adminOnly } = require('../middlewares/adminMiddleware');
 const { affectCourrierToService } = require('../controllers/affectationController');
-
+const {
+    assignCourrierToService
+} = require('../controllers/courrierController');
 const router = express.Router();
 
 router.use(protect);
 router.use(adminOnly);
 
 router.post('/:courrierId/affect', affectCourrierToService);
+
+router.patch('/:courrierId/service', assignCourrierToService);
 
 module.exports = router;
