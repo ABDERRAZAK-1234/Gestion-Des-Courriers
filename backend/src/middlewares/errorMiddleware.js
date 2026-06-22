fb3cc1e47b030131a9416787383a8b735e77bc62
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-06-22 12:59:45 +0100

    Gérer les erreurs de dépassement de taille des fichiers

--- backend/src/middlewares/errorMiddleware.js
+++ backend/src/middlewares/errorMiddleware.js
@@ -5,6 +5,12 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(413).json({
+            message: `File is too large. Maximum size is ${process.env.MAX_FILE_SIZE_MB || 20} MB`
+        });
+    }
+
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
     res.status(statusCode).json({
